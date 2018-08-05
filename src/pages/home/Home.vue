15d6eb261b173895cf99183b37434adffd8b6339
changhong <changhongwang@126.com>
2018-08-05 20:46:26 +0800

    footer component develop completed

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,12 +2,14 @@
   <div>
     <home-header></home-header>
     <navbar :links="navLinks"></navbar>
+    <home-footer></home-footer>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue'
 import navbar from '../common/navbar/Navbar.vue'
+import HomeFooter from './components/Footer.vue'
 export default {
   name: 'home',
   data () {
@@ -29,7 +31,8 @@ export default {
   },
   components: {
     HomeHeader,
-    navbar
+    navbar,
+    HomeFooter
   }
 }
 </script>
